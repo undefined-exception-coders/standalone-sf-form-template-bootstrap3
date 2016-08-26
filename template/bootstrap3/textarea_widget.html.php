7fbf774c753b94ded5518b0b9830156607f6b339
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-08-26 17:05:51 +0200

    Add form-control style class to textarea

--- template/bootstrap3/textarea_widget.html.php
+++ template/bootstrap3/textarea_widget.html.php
@@ -1,1 +1,7 @@
-<textarea <?php echo $view['form']->block($form, 'widget_attributes') ?>><?php echo $view->escape($value) ?></textarea>
+<?php
+if (!array_key_exists('class', $attr)) {
+    $attr['class'] = '';
+}
+$attr['class'] .= ' form-control';
+?>
+<textarea <?php echo $view['form']->block($form, 'widget_attributes', ['attr' => $attr]) ?>><?php echo $view->escape($value) ?></textarea>
